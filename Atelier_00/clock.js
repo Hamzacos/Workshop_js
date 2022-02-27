3fb5f1bea925e33a7327a5cb47fdec5dfe834867
hamzacos <hamza.laqraa@hotmail.com>
2022-02-27 17:02:37 +0100

    commit

--- Atelier_00/clock.js
+++ Atelier_00/clock.js
@@ -1,5 +1,4 @@
-
-        setInterval(showTime, 1000);
+ setInterval(showTime, 1000);
         function showTime() {
             let time = new Date();
             let hour = time.getHours();
@@ -9,11 +8,11 @@
   
             if (hour > 12) {
                 hour -= 12;
-                am_pm = "PM";
+                am_pm = " PM";
             }
             if (hour == 0) {
                 hr = 12;
-                am_pm = "AM";
+                am_pm = " AM";
             }
   
             hour = hour < 10 ? "0" + hour : hour;
